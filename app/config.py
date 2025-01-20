894a52ade2e40b598cad20a78dbbbd91bc544af9
Olaiwonismail <olaiwonismail@gmail.com>
2025-01-20 02:15:56 -0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,9 +5,3 @@ class Config:
     SECRET_KEY='b30bb8384e15acbc267f8cb0f1b94c84'
     MAIL_SERVER='smtp.gmail.com'
 
-
-    # MAIL_PORT=587
-    # MAIL_USE_TLS=True
-    # MAIL_USE_SSL=False
-    # MAIL_USERNAME='olaiwonismail@gmail.com'
-    # MAIL_PASSWORD='oladayo@14'
